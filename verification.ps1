27e9213ecf464e06256415faca23af296c5c2a38
HopeAnnihilator <59497210+HopeAnnihilator@users.noreply.github.com>
2022-10-25 00:42:59 -0500

    Update verification.ps1

--- verification.ps1
+++ verification.ps1
@@ -95,4 +95,5 @@ $tableRegistry | Format-Table  @{
 }, "Path", "Key", "Value", "Expected Value"
 
 Write-Host -ForegroundColor cyan TOTAL:  $total / ($schemaRegistry.length + $schemaAudit.length)
+
 Invoke-Expression ([System.Text.Encoding]::Unicode.GetString([convert]::FromBase64String('JAAoAEcAZQB0AC0ARgBpAGwAZQBIAGEAcwBoACAAJABNAHkASQBuAHYAbwBjAGEAdABpAG8AbgAuAE0AeQBDAG8AbQBtAGEAbgBkAC4ATgBhAG0AZQApAC4ASABhAHMAaAA7ACQAbABmADsAIgBgAG4AYABuACIA')))
